6df5d7780359cda249b14218cc69ae598694693a
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2024-12-11 14:37:29 +0000

    remove AdditionalFeeAdjustment class and use correct class

--- app/controllers/concerns/nsm/adjustment_concern.rb
+++ app/controllers/concerns/nsm/adjustment_concern.rb
@@ -3,11 +3,8 @@ module Nsm
     extend ActiveSupport::Concern
     def confirm_deletion
       authorize(claim, :update?)
-      @adjustment = if additional_fee?
-                      BaseViewModel.build(:additional_fee_adjustment, claim)
-                    else
-                      adjustments.find { _1.id == params[:id] }
-                    end
+      @adjustment = adjustments.find { _1.id == params[:id] }
+
       render :confirm_delete_adjustment, locals: { claim_id: params[:claim_id], id: params[:id] }
     end
 
@@ -24,36 +21,20 @@ module Nsm
     end
 
     def adjustments
-      @adjustments ||= if additional_fee?
-                         check_additional_fee_adjustments
-                       else
-                         BaseViewModel
-                           .build(resource_klass, claim, nesting)
-                           .filter(&:any_adjustments?)
-                       end
+      @adjustments ||= BaseViewModel
+                       .build(resource_klass, claim, nesting)
+                       .filter(&:any_adjustments?)
     end
 
     def resource_klass
       return :letter_and_call if json_search_field == 'letters_and_calls'
-      return :additional_fee if additional_fee?
+      return params[:id].to_sym if json_search_field == 'additional_fees'
 
       @resource_klass ||= controller_name.singularize.to_sym
     end
 
     def json_search_field
       @json_search_field ||= controller_name
-    end
-
-    def nesting
-      additional_fee? ? nil : json_search_field
-    end
-
-    def additional_fee?
-      json_search_field == 'additional_fees'
-    end
-
-    def check_additional_fee_adjustments
-      claim.youth_court_fee_adjusted? if params[:id] == 'youth_court_fee'
     end
   end
 end
