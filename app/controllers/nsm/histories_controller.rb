2f94cf45b80a991f6761772b7a831fd1bb6a5a37
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-15 11:58:41 +0000

    add controller params check to histories_controller

--- app/controllers/nsm/histories_controller.rb
+++ app/controllers/nsm/histories_controller.rb
@@ -1,5 +1,7 @@
 module Nsm
   class HistoriesController < Nsm::BaseController
+    before_action :check_controller_params
+
     def show
       authorize(claim)
       claim_summary = BaseViewModel.build(:claim_summary, claim)
@@ -11,7 +13,7 @@ module Nsm
 
     def create
       authorize(claim, :edit?)
-      claim_note = ClaimNoteForm.new(claim_note_params)
+      claim_note = ClaimNoteForm.new(form_params)
       if claim_note.save
         redirect_to nsm_claim_history_path(claim)
       else
@@ -28,10 +30,19 @@ module Nsm
       @claim ||= Claim.load_from_app_store(params[:claim_id])
     end
 
-    def claim_note_params
+    def form_params
       params.require(:nsm_claim_note_form).permit(
         :note
       ).merge(current_user:, claim:)
+    end
+
+    def controller_params
+      params.permit(:claim_id)
+    end
+
+    def check_controller_params
+      param_model = Nsm::BasicClaimParams.new(controller_params)
+      raise param_model.error_summary.to_s unless param_model.valid?
     end
   end
 end
