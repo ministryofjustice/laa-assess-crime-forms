e91648f69a3a03f7693a72d12c19af921ba67e6d
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-15 11:56:34 +0000

    Add params validation to further information downloads controller

--- app/controllers/nsm/further_information_downloads_controller.rb
+++ app/controllers/nsm/further_information_downloads_controller.rb
@@ -1,10 +1,23 @@
 module Nsm
   class FurtherInformationDownloadsController < Nsm::BaseController
+    before_action :check_controller_params
+
     include FileRedirectable
 
     def show
       authorize Claim, :show?
       redirect_to_file_download(params[:id], params[:file_name])
+    end
+
+    private
+
+    def controller_params
+      params.permit(:id, :file_name)
+    end
+
+    def check_controller_params
+      param_model = Nsm::FurtherInformationDownloadsParams.new(controller_params)
+      raise param_model.error_summary.to_s unless param_model.valid?
     end
   end
 end
