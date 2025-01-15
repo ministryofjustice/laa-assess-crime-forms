ef258f843a546617cea6b8a8fc3020f7d3c5e203
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-15 12:49:27 +0000

    add param validation to supporting evidences controller

--- app/controllers/nsm/supporting_evidences_controller.rb
+++ app/controllers/nsm/supporting_evidences_controller.rb
@@ -1,5 +1,7 @@
 module Nsm
   class SupportingEvidencesController < Nsm::BaseController
+    before_action :check_controller_params
+
     # 15 min expiry on pre-signed urls to keep evidence download as secure as possible
     PRESIGNED_EXPIRY = 900
 
@@ -10,6 +12,17 @@ module Nsm
       supporting_evidence = BaseViewModel.build(:supporting_evidence, claim, 'supporting_evidences')
       @pagy, @supporting_evidence = pagy_array(supporting_evidence)
       render locals: { claim:, claim_summary: }
+    end
+
+    private
+
+    def controller_params
+      params.permit(:claim_id)
+    end
+
+    def check_controller_params
+      param_model = Nsm::BasicClaimParams.new(controller_params)
+      raise param_model.error_summary.to_s unless param_model.valid?
     end
   end
 end
