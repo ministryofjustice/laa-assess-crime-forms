9e2351a95ece6fb94f97124fcda39c1b73dd925e
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-17 10:14:49 +0000

    use param validation for auto assignment

--- app/controllers/prior_authority/manual_assignments_controller.rb
+++ app/controllers/prior_authority/manual_assignments_controller.rb
@@ -1,5 +1,7 @@
 module PriorAuthority
   class ManualAssignmentsController < PriorAuthority::AssignmentsController
+    before_action :check_controller_params
+
     def new
       authorize(application, :assign?)
       @form = ManualAssignmentForm.new
@@ -26,7 +28,17 @@ module PriorAuthority
     end
 
     def application
-      @application ||= PriorAuthorityApplication.load_from_app_store(params[:application_id])
+      @application ||= PriorAuthorityApplication.load_from_app_store(controller_params[:application_id])
+    end
+
+
+    def controller_params
+      params.permit(:application_id)
+    end
+
+    def check_controller_params
+      param_model = PriorAuthority::BasicApplicationParams.new(controller_params)
+      raise param_model.error_summary.to_s unless param_model.valid?
     end
   end
 end
