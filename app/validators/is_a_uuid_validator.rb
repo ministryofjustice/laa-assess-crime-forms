c80190d9941e8e7cae629fa0411c9ecb8d952843
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-17 17:36:04 +0000

    undo unecessary change on uuid validator

--- app/validators/is_a_uuid_validator.rb
+++ app/validators/is_a_uuid_validator.rb
@@ -1,5 +1,5 @@
 class IsAUuidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add attribute, (options[:message] || 'is not a valid uuid') unless value.blank? || UUID.validate(value)
+    record.errors.add attribute, (options[:message] || 'is not a valid uuid') unless UUID.validate(value)
   end
 end
