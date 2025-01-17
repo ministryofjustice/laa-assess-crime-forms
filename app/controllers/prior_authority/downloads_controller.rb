85414f6676b1e820022a0131340e38808bfa37d6
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-01-17 10:10:31 +0000

    add param validation for pa - downloads

--- app/controllers/prior_authority/downloads_controller.rb
+++ app/controllers/prior_authority/downloads_controller.rb
@@ -1,10 +1,23 @@
 module PriorAuthority
   class DownloadsController < BaseController
+    before_action :check_controller_params
+
     include FileRedirectable
 
     def show
       authorize(PriorAuthorityApplication, :show?)
       redirect_to_file_download(params[:id], params[:file_name])
+    end
+
+    private
+
+    def controller_params
+      params.permit(:id, :file_name)
+    end
+
+    def check_controller_params
+      param_model = PriorAuthority::DownloadsParams.new(controller_params)
+      raise param_model.error_summary.to_s unless param_model.valid?
     end
   end
 end
