ba7c94f51012b53f218ccaa8b02bdabe80b54964
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2025-02-12 07:24:30 +0000

    Updates cost details not adjusted

--- app/view_models/nsm/v1/work_item.rb
+++ app/view_models/nsm/v1/work_item.rb
@@ -104,8 +104,8 @@ module Nsm
           '.work_type' => original_work_type.translated,
           '.date' => format_in_zone(completed_on),
           '.fee_earner' => fee_earner.to_s,
-          '.time_spent' => format_period(original_time_spent),
           '.item_rate' => NumberTo.pounds(submission.rates.work_items[original_work_type.value.to_sym]),
+          '.time_spent' => format_period(original_time_spent),
           '.uplift_claimed' => "#{original_uplift.to_i}%",
           '.total_claimed' => NumberTo.pounds(provider_requested_amount),
         }
