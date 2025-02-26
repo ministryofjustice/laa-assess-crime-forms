e6ae5d1337ecd9274f7222173c210ae9f52bc490
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2025-02-26 08:44:20 +0000

    Vat show logic updated

--- spec/system/nsm/disbursements_spec.rb
+++ spec/system/nsm/disbursements_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe 'Disbursements', :stub_oauth_token, type: :system do
         .and have_content("#{t('nsm.disbursements.show.summary_table.date')}12 December 2022")
         .and have_content("#{t('nsm.disbursements.show.summary_table.details')}Details")
         .and have_content("#{t('nsm.disbursements.show.summary_table.prior_authority')}Yes")
-        .and have_content("#{t('nsm.disbursements.show.summary_table.vat')}20%")
+        .and have_content("#{t('nsm.disbursements.show.summary_table.vat')}0%")
         .and have_content("#{t('nsm.disbursements.show.summary_table.total')}£100.00")
     end
   end
