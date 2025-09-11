f69d2a0f9ec36f415a050aab6abcc96158f5b97d
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-09-11 17:08:23 +0100

    observing decision tree and session storage wip

--- app/forms/payments/steps/claim_detail_form.rb
+++ app/forms/payments/steps/claim_detail_form.rb
@@ -1,6 +1,19 @@
 module Payments
   module Steps
     class ClaimDetailForm < BasePaymentsForm
+      attribute :ufn, :string
+      attribute :multi_step_form_session
+
+      def save
+        return false unless valid?
+
+        multi_step_form_session[:ufn] = ufn
+
+        true
+      rescue StandardError
+        errors.add(:base, :sync_error)
+        false
+      end
     end
   end
 end
