5bbf9ca150a2e65f720c403956c313e9a1154eb4
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-09-22 16:24:53 +0100

    remove more unneeded code

--- app/forms/base_form.rb
+++ app/forms/base_form.rb
@@ -2,11 +2,4 @@ class BaseForm
   include ActiveModel::Model
   include ActiveModel::Attributes
   include ActiveRecord::AttributeAssignment
-
-  # Add the ability to read/write attributes without calling their accessor methods.
-  # Needed to behave more like an ActiveRecord model, where you can manipulate the
-  # DB attributes making use of `self[:attribute]`
-  def [](attr_name)
-    public_send(attr_name)
-  end
 end
