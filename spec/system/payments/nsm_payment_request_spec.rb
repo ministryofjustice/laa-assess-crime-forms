6e4843279c3d4b65d1b14044ca4ae46c67279abc
robin skilling <robin.skilling@digital.justice.gov.uk>
2025-09-26 14:40:25 +0100

    i18ns

--- spec/system/payments/nsm_payment_request_spec.rb
+++ spec/system/payments/nsm_payment_request_spec.rb
@@ -38,25 +38,7 @@ RSpec.describe 'NSM payment request', :stub_oauth_token do
     it 'completes claim details and proceeds' do
       start_new_payment_request
       choose_claim_type("Non-Standard Magistrates'")
-
-      expect(page).to have_title('Claim details')
-
-      fill_claim_details(
-        received_on: '2025-09-24',
-        firm_office_account_number: 'asdf',
-        ufn: '120223/001',
-        prosecution_type: 'PROM',
-        defendant_first_name: 'Fred',
-        defendant_last_name: 'Flintstone',
-        defendants_count: '2',
-        attendances_count: '2',
-        hearing_outcome_code: '1234',
-        matter_type: '1234',
-        court: '1234',
-        travel_required: 'Yes',
-        work_completed_on: '2025-09-24'
-      )
-
+      fill_claim_details
       expect(page).to have_title('Claimed costs')
     end
 
@@ -64,31 +46,8 @@ RSpec.describe 'NSM payment request', :stub_oauth_token do
       it 'completes claimed costs and proceeds' do
         start_new_payment_request
         choose_claim_type("Non-Standard Magistrates'")
-
-        expect(page).to have_title('Claim details')
-
-        fill_claim_details(
-          received_on: '2025-09-24',
-          firm_office_account_number: 'asdf',
-          ufn: '120223/001',
-          prosecution_type: 'PROM',
-          defendant_first_name: 'Fred',
-          defendant_last_name: 'Flintstone',
-          defendants_count: '2',
-          attendances_count: '2',
-          hearing_outcome_code: '1234',
-          matter_type: '1234',
-          court: '1234',
-          travel_required: 'Yes',
-          work_completed_on: '2025-09-24'
-        )
-
-        expect(page).to have_title('Claimed costs')
-
-        fill_costs(profit_costs: 10,
-                   disbursement_costs: 10,
-                   travel_costs: 10,
-                   waiting_costs: 10)
+        fill_claim_details
+        fill_claimed_costs
         expect(page).to have_title('Allowed costs')
       end
     end
@@ -97,39 +56,9 @@ RSpec.describe 'NSM payment request', :stub_oauth_token do
       it 'completes claimed costs and proceeds' do
         start_new_payment_request
         choose_claim_type("Non-Standard Magistrates'")
-
-        expect(page).to have_title('Claim details')
-
-        fill_claim_details(
-          received_on: '2025-09-24',
-          firm_office_account_number: 'asdf',
-          ufn: '120223/001',
-          prosecution_type: 'PROM',
-          defendant_first_name: 'Fred',
-          defendant_last_name: 'Flintstone',
-          defendants_count: '2',
-          attendances_count: '2',
-          hearing_outcome_code: '1234',
-          matter_type: '1234',
-          court: '1234',
-          travel_required: 'Yes',
-          work_completed_on: '2025-09-24'
-        )
-
-        expect(page).to have_title('Claimed costs')
-
-        fill_costs(profit_costs: 10,
-                   disbursement_costs: 10,
-                   travel_costs: 10,
-                   waiting_costs: 10)
-
-        expect(page).to have_title('Allowed costs')
-
-        fill_costs(profit_costs: 10,
-                   disbursement_costs: 10,
-                   travel_costs: 10,
-                   waiting_costs: 10)
-
+        fill_claim_details
+        fill_claimed_costs
+        fill_allowed_costs
         expect(page).to have_title('Check your answers')
       end
     end
