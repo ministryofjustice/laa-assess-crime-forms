9dd7d495fb027fdb9277f53b7e1468c398803465
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-10-08 10:49:55 +0100

    use consistent speech marks

--- app/view_models/payments/claim_details.rb
+++ app/view_models/payments/claim_details.rb
@@ -7,39 +7,39 @@ module Payments
     end
 
     def id
-      @payment_request_claim['id']
+      @payment_request_claim["id"]
     end
 
     def claim_type
-      I18n.t("shared.claim_type.#{@payment_request_claim['type']}")
+      I18n.t("shared.claim_type.#{@payment_request_claim["type"]}")
     end
 
     def date_received
-      DateTime.parse(@payment_request_claim['date_received']).to_fs(:stamp)
+      DateTime.parse(@payment_request_claim["date_received"]).to_fs(:stamp)
     end
 
     def office_code
-      @payment_request_claim['office_code']
+      @payment_request_claim["office_code"]
     end
 
     def firm_name
-      @payment_request_claim['firm_name']
+      @payment_request_claim["firm_name"]
     end
 
     def ufn
-      @payment_request_claim['ufn']
+      @payment_request_claim["ufn"]
     end
 
     def stage_code
-      @payment_request_claim['stage_code']
+      @payment_request_claim["stage_code"]
     end
 
     def defendant_first_name
-      @payment_request_claim['client_first_name']
+      @payment_request_claim["client_first_name"]
     end
 
     def defendant_last_name
-      @payment_request_claim['client_last_name']
+      @payment_request_claim["client_last_name"]
     end
 
     def defendant_name
@@ -47,39 +47,39 @@ module Payments
     end
 
     def court_attendances
-      @payment_request_claim['court_attendances']
+      @payment_request_claim["court_attendances"]
     end
 
     def no_of_defendants
-      @payment_request_claim['no_of_defendants']
+      @payment_request_claim["no_of_defendants"]
     end
 
     def outcome_code
-      LaaCrimeFormsCommon::OutcomeCode.new(@payment_request_claim['outcome_code']).name
+      LaaCrimeFormsCommon::OutcomeCode.new(@payment_request_claim["outcome_code"]).name
     end
 
     def matter_type
-      LaaCrimeFormsCommon::MatterType.new(@payment_request_claim['matter_type']).name
+      LaaCrimeFormsCommon::MatterType.new(@payment_request_claim["matter_type"]).name
     end
 
     def court
-      @payment_request_claim['court_name']
+      @payment_request_claim["court_name"]
     end
 
     def youth_court
-      @payment_request_claim['youth_court'] ? 'Yes' : 'No'
+      @payment_request_claim["youth_court"] ? "Yes" : "No"
     end
 
     def laa_reference
-      @payment_request_claim['laa_reference']
+      @payment_request_claim["laa_reference"]
     end
 
     def last_updated
-      DateTime.parse(@payment_request_claim['updated_at']).to_fs(:stamp)
+      DateTime.parse(@payment_request_claim["updated_at"]).to_fs(:stamp)
     end
 
     def payment_requests
-      @payment_requests ||= @payment_request_claim['payment_requests']
+      @payment_requests ||= @payment_request_claim["payment_requests"]
                             .map { Payments::PaymentRequestDetails.new(_1) }
                             .sort_by(&:date_completed).reverse
     end
@@ -89,32 +89,39 @@ module Payments
     end
 
     def work_completed_date
-      DateTime.parse(@payment_request_claim['work_completed_date']).to_fs(:stamp)
+      DateTime.parse(@payment_request_claim["work_completed_date"]).to_fs(:stamp)
     end
 
     def original_claim
-      link_to("Link to: #{laa_reference}", '')
+      reference = AppStoreClient.new(@payment_request_claim["laa_reference"])
+      if reference["type"] == "Submission"
+        link_to("Link to: #{laa_reference}", )
+      else
+        nil
+      end
     end
 
     def table_format
       [
-        [table_heading('original_claim'), { text: original_claim, numeric: false }],
-        [table_heading('claim_type'), { text: claim_type, numeric: false }],
-        [table_heading('date_received'), { text: date_received, numeric: false }],
-        [table_heading('office_code'), { text: office_code, numeric: false }],
-        [table_heading('firm_name'), { text: firm_name, numeric: false }],
-        [table_heading('ufn'), { text: ufn, numeric: false }],
-        [table_heading('stage_code'), { text: stage_code, numeric: false }],
-        [table_heading('defendant_name'), { text: defendant_name, numeric: false }],
-        [table_heading('no_of_defendants'), { text: no_of_defendants, numeric: false }],
-        [table_heading('court_attendances'), { text: court_attendances, numeric: false }],
-        [table_heading('outcome_code'), { text: outcome_code, numeric: false }],
-        [table_heading('matter_type'), { text: matter_type, numeric: false }],
-        [table_heading('court'), { text: court, numeric: false }],
-        [table_heading('youth_court'), { text: youth_court, numeric: false }],
-        [table_heading('work_completed_date'), { text: work_completed_date, numeric: false }],
-      ]
+        original_claim ? [table_heading("original_claim"), { text: original_claim, numeric: false }] : nil,
+        [table_heading("claim_type"), { text: claim_type, numeric: false }],
+        [table_heading("date_received"), { text: date_received, numeric: false }],
+        [table_heading("office_code"), { text: office_code, numeric: false }],
+        [table_heading("firm_name"), { text: firm_name, numeric: false }],
+        [table_heading("ufn"), { text: ufn, numeric: false }],
+        [table_heading("stage_code"), { text: stage_code, numeric: false }],
+        [table_heading("defendant_name"), { text: defendant_name, numeric: false }],
+        [table_heading("no_of_defendants"), { text: no_of_defendants, numeric: false }],
+        [table_heading("court_attendances"), { text: court_attendances, numeric: false }],
+        [table_heading("outcome_code"), { text: outcome_code, numeric: false }],
+        [table_heading("matter_type"), { text: matter_type, numeric: false }],
+        [table_heading("court"), { text: court, numeric: false }],
+        [table_heading("youth_court"), { text: youth_court, numeric: false }],
+        [table_heading("work_completed_date"), { text: work_completed_date, numeric: false }],
+    ].compact
     end
+
+    private
 
     def table_heading(key)
       I18n.t("payments.requests.claim_details.table.#{key}")
