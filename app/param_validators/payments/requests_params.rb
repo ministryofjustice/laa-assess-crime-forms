cf3dffe93d653ab5b24c29ea48aff8ba058244ed
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-10-23 14:52:10 +0100

    fix linting error

--- app/param_validators/payments/requests_params.rb
+++ app/param_validators/payments/requests_params.rb
@@ -7,8 +7,10 @@ module Payments
     attribute :current_page, :string
     attribute :payment_id, :string
 
-    validates :sort_by, inclusion: { in: %w[laa_reference firm_name solicitor_firm_name client_last_name request_type submitted_at] },
-  allow_nil: true
+    validates :sort_by, inclusion: { in: %w[
+      laa_reference firm_name solicitor_firm_name
+      client_last_name request_type submitted_at
+    ] }, allow_nil: true
     validates :sort_direction, inclusion: { in: %w[ascending descending] }, allow_nil: true
     validates :page, numericality: { greater_than: 0 }, allow_nil: true
     validates :id, is_a_uuid: true, allow_nil: true
