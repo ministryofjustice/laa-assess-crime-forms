c081ea874c69880391fe2a7cc469f3b337b84371
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-12-11 14:20:43 +0000

    add request_type to BasePaymentsForm and use that instead of instance vars

--- app/controllers/payments/steps/date_received_controller.rb
+++ app/controllers/payments/steps/date_received_controller.rb
@@ -2,7 +2,6 @@ module Payments
   module Steps
     class DateReceivedController < BaseController
       def edit
-        @request_type = multi_step_form_session.answers['request_type']
         @form_object = Payments::Steps::DateReceivedForm.build(multi_step_form_session.answers, multi_step_form_session:)
       end
 
