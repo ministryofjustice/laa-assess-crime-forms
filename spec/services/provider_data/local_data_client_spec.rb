036344b48e45cd4ea9c9babc28ef825c29864d30
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2025-12-17 14:29:11 +0000

    add tests for local data client

---
+++ spec/services/provider_data/local_data_client_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe ProviderData::LocalDataClient do
+  describe '#office_details' do
+    let(:office_code) { nil }
+    let(:subject) { described_class.new.office_details(office_code) }
+
+    context 'when office code is 1A123B' do
+      let(:office_code) { '1A123B' }
+
+      it 'returns the correct office details' do
+        expect(subject).to eq({
+          'firmOfficeId' => 1,
+          'ccmsFirmOfficeId' => 1,
+          'firmOfficeCode' => '1A123B',
+          'officeName' => 'Firm & Sons',
+          'officeCodeAlt' => '1A123B',
+          'type' => 'Solicitor'
+        })
+      end
+    end
+
+    context 'when office code is something else' do
+      let(:office_code) { 'XYZ789' }
+      it 'returns nil' do
+        expect(subject).to be_nil
+      end
+    end
+  end
+end
