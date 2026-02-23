4677955a5f47c9caeb74e18eb9c95166a7f86b38
ivanELEC <ivan.yohuno@digital.justice.gov.uk>
2026-02-23 10:15:04 +0000

    use suggestion select to show full court and id but save them seperately

--- app/forms/payments/steps/nsm/claim_detail_form.rb
+++ app/forms/payments/steps/nsm/claim_detail_form.rb
@@ -13,12 +13,14 @@ module Payments
         attribute :number_of_attendances, :integer
         attribute :hearing_outcome_code, :string
         attribute :matter_type, :string
-        attribute :court, :string
+        attribute :court_name, :string
+        attribute :court_id, :string
+        attribute :court_name_suggestion, :string
         attribute :youth_court, :boolean
         attribute :date_completed, :date
 
         validates :defendant_first_name, :defendant_last_name,
-                  :hearing_outcome_code, :matter_type, :court,
+                  :hearing_outcome_code, :matter_type, :court_name,
                   presence: true
 
         validates :number_of_defendants, :number_of_attendances, presence: true, is_a_number: true,
@@ -32,6 +34,19 @@ module Payments
         validates :ufn, presence: true, ufn: true
         validates :date_completed, :date_received,
                   presence: true, multiparam_date: { allow_past: true, allow_future: false }
+
+        def self.build(form_data, multi_step_form_session:)
+          if form_data['court_name_suggestion'].in?(LaaCrimeFormsCommon::Court.all.map(&:name))
+            record = LaaCrimeFormsCommon::Court.all.find { |court| court.name == form_data['court_name_suggestion'] }
+            form_data['court_id'] = record.id
+            form_data['court_name'] = record.short_name
+          else
+            form_data['court_id'] = 'custom'
+            form_data['court_name'] = form_data['court_name_suggestion']
+          end
+          attrs = form_data.slice(*attribute_names).merge!(multi_step_form_session:)
+          new(attrs)
+        end
       end
     end
   end
