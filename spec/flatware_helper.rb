031f3e309e8d6569bb58d8edee998b93462d78de
robin skilling <robin.skilling@digital.justice.gov.uk>
2026-03-02 16:50:49 +0000

    run sans flatware

--- spec/flatware_helper.rb
+++ spec/flatware_helper.rb
@@ -1,22 +1,26 @@
 ENV['PGGSSENCMODE'] = 'disable'
 require 'flatware/rspec'
 
-Flatware.configure do |conf|
-  conf.before_fork do
-    require 'rails_helper'
+# flatware is only available when specs are launched via `flatware`; guard so
+# plain `rspec` runs don't blow up.
+if Flatware.respond_to?(:configure)
+  Flatware.configure do |conf|
+    conf.before_fork do
+      require 'rails_helper'
 
-    ActiveRecord::Base.connection.disconnect!
-  end
+      ActiveRecord::Base.connection.disconnect!
+    end
 
-  conf.after_fork do |test_env_number|
-    SimpleCov.at_fork.call(test_env_number)
+    conf.after_fork do |test_env_number|
+      SimpleCov.at_fork.call(test_env_number)
 
-    config = ActiveRecord::Base.connection_db_config.configuration_hash
+      config = ActiveRecord::Base.connection_db_config.configuration_hash
 
-    ActiveRecord::Base.establish_connection(
-      config.merge(
-        database: config.fetch(:database) + test_env_number.to_s
+      ActiveRecord::Base.establish_connection(
+        config.merge(
+          database: config.fetch(:database) + test_env_number.to_s
+        )
       )
-    )
+    end
   end
 end
