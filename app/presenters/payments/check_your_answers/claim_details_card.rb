d30039a50c1660c9f08be785576ebcd259b354eb
Christopher Rose <Christopher.Rose1@justice.gov.uk>
2026-03-04 14:27:28 +0000

    CRM457-2903: Update CYA change path by linked claim state

--- app/presenters/payments/check_your_answers/claim_details_card.rb
+++ app/presenters/payments/check_your_answers/claim_details_card.rb
@@ -115,11 +115,28 @@ module Payments
       end
 
       def change_link_controller_path
-        "payments/steps/nsm/#{section}"
+        if linked_claim?
+          'payments/steps/claim_search'
+        else
+          'payments/steps/office_code_search'
+        end
       end
 
       def read_only?
-        session_answers['linked_laa_reference'].present?
+        false
+      end
+
+      private
+
+      def linked_claim?
+        case session_answers['request_type']
+        when 'non_standard_magistrate', 'breach_of_injunction'
+          session_answers['linked_laa_reference'].present?
+        when 'non_standard_mag_supplemental', 'non_standard_mag_appeal', 'non_standard_mag_amendment'
+          session_answers['laa_reference'].present?
+        else
+          false
+        end
       end
     end
   end
