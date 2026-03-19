8def0a4e9c2af3aa9c5fd3991c2e4047237e8377
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-03-19 16:35:59 +0000

    ensure court details get correctly formatted into claim details format

--- app/models/payments/selected_claim_transformer.rb
+++ app/models/payments/selected_claim_transformer.rb
@@ -3,8 +3,24 @@ module Payments
     private
 
     def claim
-      @claim ||= AppStoreClient.new.get_payable_claim(payable_claim_id)
-                               .deep_stringify_keys.with_indifferent_access
+      @claim ||= raw_claim.merge('court_name' => court_name, 'court_id' => court_id).except('court')
+    end
+
+    def raw_claim
+      @raw_claim ||= AppStoreClient.new.get_payable_claim(payable_claim_id)
+                                    .deep_stringify_keys.with_indifferent_access
+    end
+
+    def court_item
+      LaaCrimeFormsCommon::Court.all.find { |c| @raw_claim['court'].downcase == c.name.downcase }
+    end
+
+    def court_id
+      court_item&.id || I18n.t('laa_crime_forms_common.shared.custom')
+    end
+
+    def court_name
+      court_item&.short_name || @raw_claim['court'].delete_suffix(" - #{I18n.t('laa_crime_forms_common.shared.na')}")
     end
   end
 end
