b9348e5212d4d3d51cbd9c76553a5f762a007db4
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-03-23 13:55:21 +0000

    Revert changes to SelectedClaimTransformer and tests

--- app/models/payments/selected_claim_transformer.rb
+++ app/models/payments/selected_claim_transformer.rb
@@ -3,28 +3,8 @@ module Payments
     private
 
     def claim
-      @claim ||= if raw_claim['type'] == 'NsmClaim'
-                   raw_claim.merge('court_name' => court_name, 'court_id' => court_id).except('court')
-                 else
-                   raw_claim
-                 end
-    end
-
-    def raw_claim
-      @raw_claim ||= AppStoreClient.new.get_payable_claim(payable_claim_id)
-                                   .deep_stringify_keys.with_indifferent_access
-    end
-
-    def court_item
-      LaaCrimeFormsCommon::Court.all.find { |c| @raw_claim['court'].downcase == c.name.downcase }
-    end
-
-    def court_id
-      court_item&.id || I18n.t('laa_crime_forms_common.shared.custom')
-    end
-
-    def court_name
-      court_item&.short_name || @raw_claim['court'].delete_suffix(" - #{I18n.t('laa_crime_forms_common.shared.na')}")
+      @claim ||= AppStoreClient.new.get_payable_claim(payable_claim_id)
+                               .deep_stringify_keys.with_indifferent_access
     end
   end
 end
