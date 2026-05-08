975006b692d0bf8c8fe58b629e5c4a9504e1cb3c
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-05-08 10:51:28 +0100

    Fix logic for when original allowed total is shown for AC appeals/amendments

--- app/view_models/payments/costs_summary_amended.rb
+++ app/view_models/payments/costs_summary_amended.rb
@@ -11,7 +11,7 @@ module Payments
     def headers
       [
         '',
-        (t('original_total_allowed') if session_answers['claimed_total'].present?),
+        (t('original_total_allowed') if session_answers['original_allowed_total'].present?),
         t('total_allowed')
       ].compact
     end
@@ -29,7 +29,7 @@ module Payments
     def formatted_summed_fields
       {
         name: t('total', numeric: false),
-        original_total_allowed: (if session_answers['claimed_total'].present?
+        original_total_allowed: (if session_answers['original_allowed_total'].present?
                                    format(session_answers['original_allowed_total'].to_f)
                                  end),
         total_allowed: format(session_answers['allowed_total'].to_f)
@@ -41,7 +41,7 @@ module Payments
     def build_row(type)
       {
         name: t(type, numeric: false),
-        original_total_allowed: (if session_answers['claimed_total'].present?
+        original_total_allowed: (if session_answers["original_allowed_#{type}"].present?
                                    format(session_answers["original_allowed_#{type}"].to_f)
                                  end),
         total_allowed: format(session_answers["allowed_#{type}"].to_f)
