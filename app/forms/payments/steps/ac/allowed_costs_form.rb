27d31ba08a1f49232d37f933bdfd9b43fed7eb2a
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-05-08 14:18:56 +0100

    allow negative allowed costs for amendments

--- app/forms/payments/steps/ac/allowed_costs_form.rb
+++ app/forms/payments/steps/ac/allowed_costs_form.rb
@@ -2,13 +2,16 @@ module Payments
   module Steps
     module Ac
       class AllowedCostsForm < BasePaymentsForm
+        include NumericLimits
+
         attribute :allowed_net_assigned_counsel_cost, :gbp
         attribute :allowed_assigned_counsel_vat, :gbp
         attribute :allowed_total, :gbp
 
         validates :allowed_net_assigned_counsel_cost, :allowed_assigned_counsel_vat,
-                  presence: true, numericality: { greater_than_or_equal_to: 0 }, is_a_number: true
-
+                  presence: true, numericality: { less_than_or_equal_to: NumericLimits::MAX_FLOAT }, is_a_number: true
+        validates :allowed_net_assigned_counsel_cost, :allowed_assigned_counsel_vat,
+                  numericality: { greater_than_or_equal_to: 0 }, if: -> { not_amendment? }
         def save
           return false unless valid?
 
@@ -19,6 +22,12 @@ module Payments
           multi_step_form_session[:allowed_total] = calculated_costs
 
           true
+        end
+
+        private
+
+        def not_amendment?
+          multi_step_form_session['request_type'] != 'assigned_counsel_amendment'
         end
       end
     end
