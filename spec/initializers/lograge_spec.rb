2b6a3a8a311433f11af59d49fe9553f51d978ae4
Christopher Rose <Christopher.Rose1@justice.gov.uk>
2026-07-13 10:02:27 +0100

    CRM457-2512: Normalise outbound request IDs

--- spec/initializers/lograge_spec.rb
+++ spec/initializers/lograge_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe Lograge do
     user = instance_double(User, to_param: 'caseworker-id')
     controller = instance_double(ApplicationController, current_user: user)
 
-    OutboundRequestId.set('rails-request-id')
+    OutboundRequestId.set('A8B0EB88-EA9A-DCAB-8902-CD521F2D5F51')
 
     expect(Rails.application.config.lograge.custom_payload_method.call(controller)).to eq(
       caseworker_id: 'caseworker-id',
-      request_id: 'nscc-assess-rails-request-id'
+      request_id: 'nscc-assess-a8b0eb88ea9adcab8902cd521f2d5f51'
     )
   end
 end
