57cf7bf29268c39e5dbeb6af9a8b0692e0ecc881
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-07-21 14:00:56 +0100

    Add log to indicate task run

--- app/jobs/payments/schedule_finance_reports.rb
+++ app/jobs/payments/schedule_finance_reports.rb
@@ -3,6 +3,7 @@ module Payments
     sidekiq_options retry: 5
 
     def perform
+      Rails.logger.info "Running Payments::ScheduleFinanceReports at #{Time.zone.now}"
       EmailToFinanceMailer.notify('start_date', 'end_date').deliver_now
     end
   end
