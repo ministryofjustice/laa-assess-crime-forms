ffb5c2b5b6abd821f3957e7e4673a1c203c1e8aa
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-07-22 16:57:46 +0100

    use binary mode for file

--- app/mailers/payments/email_to_finance_mailer.rb
+++ app/mailers/payments/email_to_finance_mailer.rb
@@ -5,18 +5,23 @@ module Payments
     def notify(start_date, end_date)
       message = instantiate_message(start_date, end_date)
       set_template(message.template)
-      file = Tempfile.new(['finance_report', '.csv'])
-      file.write(MetabaseApiClient.new.download_question(278, @start_date, @end_date))
-      file.close
+      tmpfile = construct_file(start_date, end_date)
       set_personalisation(**message.contents(file))
       mail(to: message.recipient)
-      file.unlink
+      tmpfile.unlink
     end
 
     private
 
     def instantiate_message(start_date, end_date)
       Payments::Messages::FinanceReport.new(start_date, end_date)
+    end
+
+    def construct_file(start_date, end_date)
+      file = Tempfile.new(['finance_report', '.csv'], binmode: true)
+      file.write(MetabaseApiClient.new.download_question(278, start_date, end_date))
+      file.close
+      file
     end
   end
 end
