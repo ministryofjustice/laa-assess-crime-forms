74bfa3f46f8a2ac2c81c1e496a0907376f26dfe5
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-07-24 11:09:03 +0100

    Fix incorrect file handling

--- app/mailers/payments/messages/finance_report.rb
+++ app/mailers/payments/messages/finance_report.rb
@@ -23,8 +23,8 @@ module Payments
 
       def contents
         filename = "finance_report_#{@start_date}_to_#{@end_date}.csv"
-        file = prepare_file(filename)
-        file.open do |file|
+        file_path = prepare_file(filename)
+        File.open(file_path, 'rb') do |file|
           {
             start_date: @start_date,
             end_date: @end_date,
@@ -43,6 +43,7 @@ module Payments
         file_path = File.join(@directory_path, filename)
         csv_download = MetabaseApiClient.new.download_question(278, @start_date, @end_date)
         File.binwrite(file_path, csv_download)
+        file_path
       end
     end
   end
