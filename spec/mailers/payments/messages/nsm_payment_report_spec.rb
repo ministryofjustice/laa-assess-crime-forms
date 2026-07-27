976d7bc236c089045e4c9bda361d5e3a7d799e6e
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-07-27 15:22:38 +0100

    add test for payment messages for when the period of file already exists

--- spec/mailers/payments/messages/nsm_payment_report_spec.rb
+++ spec/mailers/payments/messages/nsm_payment_report_spec.rb
@@ -17,12 +17,27 @@ RSpec.describe Payments::Messages::NsmPaymentReport do
   end
 
   describe '#contents' do
-    it 'generates the correct content hash' do
-      expect(subject.contents).to eq({
-                                       start_date: start_date,
-        end_date: end_date,
-        link_to_file: attach_link
-                                     })
+    context 'when the report file does not exist' do
+      it 'prepares the file and generates the correct content hash' do
+        expect(File).to receive(:exist?).and_return(false)
+        expect(subject.contents).to eq({
+                                         start_date: start_date,
+                                         end_date: end_date,
+                                         link_to_file: attach_link
+                                       })
+      end
+    end
+
+    context 'when the report file already exists' do
+      it 'does not prepare the file again and generates the correct content hash' do
+        expect(File).to receive(:exist?).and_return(true)
+        expect(MetabaseApiClient).not_to receive(:new)
+        expect(subject.contents).to eq({
+                                         start_date: start_date,
+                                         end_date: end_date,
+                                         link_to_file: attach_link
+                                       })
+      end
     end
   end
 end
