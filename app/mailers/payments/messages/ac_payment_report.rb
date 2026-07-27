1a7e6e986652c87c6391a7840981419e149394f1
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-07-27 17:41:54 +0100

    update to correct usage of env for email template ids

--- app/mailers/payments/messages/ac_payment_report.rb
+++ app/mailers/payments/messages/ac_payment_report.rb
@@ -4,7 +4,7 @@ module Payments
   module Messages
     class AcPaymentReport < Base
       def template
-        '29db3a67-a5c0-454c-bdc4-0a8b0b9958a8'
+        ENV.fetch('AC_REPORT_EMAIL_TEMPLATE_ID', nil)
       end
 
       def report_name
