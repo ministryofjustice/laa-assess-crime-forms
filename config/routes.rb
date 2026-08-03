f8230d77dc9e90180821d19dfbf312534d57cf66
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-08-03 16:22:09 +0100

    update additional routes for office code search pages

--- config/routes.rb
+++ config/routes.rb
@@ -173,10 +173,10 @@ Rails.application.routes.draw do
 
           edit_step :check_your_answers
           edit_step :date_claim_assessed
-          edit_step :office_code_search
-          edit_step :office_code_confirm
-          edit_step :counsel_code_search
-          edit_step :counsel_code_confirm
+          edit_step :office_code_search, path: 'solicitor-account-number'
+          edit_step :office_code_confirm, path: 'check-solicitor-details'
+          edit_step :counsel_code_search, path: 'assigned-counsel-account-number'
+          edit_step :counsel_code_confirm, path: 'check-assigned-counsel-details'
           resource :claim_search, path: 'find-a-claim', only: %i[new edit update],
             controller: 'claim_search', path_names: { edit: '' }
           edit_step :claim_types
