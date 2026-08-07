632afd77f5c5ecf14b79d184a9a8774bffb03220
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-08-07 08:43:46 +0100

    Add submission allowed costs controller to back to cya loop flow

--- app/helpers/decision_step_header_helper.rb
+++ app/helpers/decision_step_header_helper.rb
@@ -12,7 +12,7 @@ module DecisionStepHeaderHelper
   def as
     current_request = request.path_parameters[:controller].split('/').last
     current_referer = URI.parse(request.referer).path.split('/').last
-    back_to_cya = %w[claim_search claimed_costs allowed_costs office_code_search]
+    back_to_cya = %w[claim_search claimed_costs allowed_costs submission_allowed_costs office_code_search]
 
     if back_to_cya.include?(current_request) && current_referer == 'check_your_answers'
       'change_your_answers'
