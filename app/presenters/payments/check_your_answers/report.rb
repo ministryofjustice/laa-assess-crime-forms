2001a83b2492dcc89f1736cae4281e02652bdd6c
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-09-15 09:52:58 +0100

    Move cost summary generation logic to a separate service

--- app/presenters/payments/check_your_answers/report.rb
+++ app/presenters/payments/check_your_answers/report.rb
@@ -63,38 +63,9 @@ module Payments
         # :nocov:
       end
 
-      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
       def cost_summary
-        if session_answers['request_type'].start_with?('non_standard_mag') && to_be_paid
-          Payments::NsmCostsSummaryToBePaid.new(session_answers)
-        elsif session_answers['request_type'].start_with?('assigned_counsel') && to_be_paid
-          Payments::AcCostsSummaryToBePaid.new(session_answers)
-        else
-          case session_answers['request_type'].to_sym
-          when :non_standard_magistrate, :breach_of_injunction
-            Payments::NsmCostsSummary.new(session_answers, from_submission: from_submission?)
-          when :non_standard_mag_supplemental
-            if session_answers['laa_reference'].present? || session_answers['linked_laa_reference'].present?
-              Payments::NsmCostsSummaryAmendedAndClaimed.new(session_answers)
-            else
-              Payments::NsmCostsSummary.new(session_answers)
-            end
-          when :non_standard_mag_amendment, :non_standard_mag_appeal
-            Payments::NsmCostsSummaryAmended.new(session_answers)
-          when :assigned_counsel
-            Payments::AcCostsSummary.new(session_answers)
-          when :assigned_counsel_appeal
-            Payments::AcCostsSummaryAppealed.new(session_answers)
-          when :assigned_counsel_amendment
-            Payments::AcCostsSummaryAmended.new(session_answers)
-          # :nocov:
-          else
-            raise StandardError, "Unknown request type: #{session_answers['request_type']}"
-          end
-          # :nocov:
-        end
+        Payments::CostSummaryService.new(session_answers, to_be_paid).call
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
       private
 
