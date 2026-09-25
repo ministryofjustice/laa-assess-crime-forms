d2719795e134238b5cdb8f8dda348d35f9195c53
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2026-09-25 10:02:22 +0100

    fix incorrect router helper method

--- app/view_models/payments/nsm_costs_summary_to_be_paid.rb
+++ app/view_models/payments/nsm_costs_summary_to_be_paid.rb
@@ -19,7 +19,7 @@ module Payments
     end
 
     def change_link
-      url_helpers.edit_payments_steps_nsm_submission_allowed_costs_path(session_answers['id'])
+      url_helpers.edit_payments_steps_nsm_allowed_costs_path(session_answers['id'])
     end
 
     private
